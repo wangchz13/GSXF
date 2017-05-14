18aa3e5132518bd0401b0dfab08296b8c17a5678
wangchz13 <wangchz13@gmail.com>
2017-05-14 10:35:51 +0800

    -

---
+++ GSXF.Web/Controllers/AdminController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GSXF.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 执业申请
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ZYSQ()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 项目登记
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult XMDJ()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 图表展示
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TBZS()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 评价页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PJYM()
+        {
+            return View();
+        }
+        //!---------------------------------------------------------------------------------
+        /// <summary>
+        /// 消防机构首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index_XFJG()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 项目监管
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult XMJG()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 报告备案
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BGBA()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 项目查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult XMCX()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 省内机构
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SNJG()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 省外机构
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SWJG()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 机构备案
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult JGBA()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 服务质量
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult FWZL()
+        {
+            return View();
+        }
+        public ActionResult FWZL2()
+        {
+            return View();
+        }
+        public ActionResult FWZL3()
+        {
+            return View();
+        }
+        public ActionResult FWZL4()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index_FWJG()
+        {
+            return View();
+        }
+
+        public ActionResult DJXM()
+        {
+            return View();
+        }
+
+        public ActionResult CXXM()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 人员备案
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RYBA()
+        {
+            return View();
+        }
+    }
+}
---
+++ GSXF.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GSXF.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Default()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult JGCX()
+        {
+            return View();
+        }
+
+        public ActionResult XMCX()
+        {
+            return View();
+        }
+    }
+}
