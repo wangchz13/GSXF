626eeb59e325cfa05005289b219a73c90c95e6ae
wangchz13 <wangchz13@gmail.com>
2017-05-18 17:07:42 +0800

    提交数据成功

--- GSXF.Web/Controllers/HomeController.cs
+++ GSXF.Web/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace GSXF.Web.Controllers
     public class HomeController : Controller
     {
         private static ArticleManager articleManager = new ArticleManager();
-        
+        private static EmployeeManager employeeManager = new EmployeeManager();
         public ActionResult Default()
         {
             return View();
@@ -41,6 +41,17 @@ namespace GSXF.Web.Controllers
         public ActionResult XMCX()
         {
             return View();
+        }
+
+        public ActionResult AddEmployee(List<Employee> list)
+        {
+            string result = "";
+            foreach(var i in list)
+            {
+                employeeManager.Add(i);
+                
+            }
+            return Json(new { rows = list.Count.ToString(), result = result });
         }
     }
 }
