fe7e7645f29ac1e3959751c48b274f40e997b77d
wangchz13 <wangchz13@gmail.com>
2017-05-18 12:59:07 +0800

    直接使用汉字作为枚举成员，方便转json

--- GSXF.Core/EnumItem.cs
+++ GSXF.Core/EnumItem.cs
@@ -1,194 +1,105 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GSXF.Core
 {
+
     /// <summary>
     /// 资质类型
     /// </summary>
     public enum QualificationType
     {
-        /// <summary>
-        /// 维修保养检测
-        /// </summary>
-        WXBYJC,
-        /// <summary>
-        /// 消防安全评估
-        /// </summary>
-        XFAQPG
+        维修保养检测,
+        消防安全评估
     }
+
     /// <summary>
     /// 资质等级
     /// </summary>
     public enum QualificationLevel
     {
-        /// <summary>
-        /// 一级
-        /// </summary>
-        YJ,
-        /// <summary>
-        /// 二级
-        /// </summary>
-        EJ,
-        /// <summary>
-        /// 三级
-        /// </summary>
-        SJ,
-        /// <summary>
-        /// 临时一级
-        /// </summary>
-        LSYJ,
-        /// <summary>
-        /// 临时二级
-        /// </summary>
-        LSEJ,
-        /// <summary>
-        /// 临时三级
-        /// </summary>
-        LSSJ
+        一级,
+        二级,
+        三级,
+        临时一级,
+        临时二级,
+        临时三级
     }
+
     /// <summary>
     /// 项目类型
     /// </summary>
     public enum ProjectType
     {
-        /// <summary>
-        /// 竣工检测
-        /// </summary>
-        JGJC,
-        /// <summary>
-        /// 年度检测
-        /// </summary>
-        NDJC,
-        /// <summary>
-        /// 维护保养
-        /// </summary>
-        WHBY,
-        /// <summary>
-        /// 安全评估
-        /// </summary>
-        AQPG
+        竣工检测,
+        年度检测,
+        维护保养,
+        安全评估
     }
+
     /// <summary>
     /// 项目进度
     /// </summary>
     public enum ProjectProgress
     {
-        /// <summary>
-        /// 项目登记
-        /// </summary>
-        XMDJ,
-        /// <summary>
-        /// 入场检测
-        /// </summary>
-        RCJC,
-        /// <summary>
-        /// 出具报告
-        /// </summary>
-        CJBG,
-        /// <summary>
-        /// 提交备案
-        /// </summary>
-        TJBA
+        项目登记,
+        入场检测,
+        出具报告,
+        提交备案
     }
+
     /// <summary>
     /// 十五个地区
     /// </summary>
     public enum City
     {
-        /// <summary>
-        /// 兰州
-        /// </summary>
-        LZ,
-        /// <summary>
-        /// 天水
-        /// </summary>
-        TS,
-        /// <summary>
-        /// 金昌
-        /// </summary>
-        JC,
-        /// <summary>
-        /// 白银
-        /// </summary>
-        BY,
-        /// <summary>
-        /// 武威
-        /// </summary>
-        WW,
-        /// <summary>
-        /// 平凉
-        /// </summary>
-        PL,
-        /// <summary>
-        /// 金昌
-        /// </summary>
-        JQ,
-        /// <summary>
-        /// 庆阳
-        /// </summary>
-        QY,
-        /// <summary>
-        /// 定西
-        /// </summary>
-        DX,
-        /// <summary>
-        /// 陇南
-        /// </summary>
-        LN,
-        /// <summary>
-        /// 临夏
-        /// </summary>
-        LX,
-        /// <summary>
-        /// 嘉峪关
-        /// </summary>
-        JYG,
-        /// <summary>
-        /// 甘南
-        /// </summary>
-        GN,
-        /// <summary>
-        /// 张掖
-        /// </summary>
-        ZY,
-        /// <summary>
-        /// 兰州新区
-        /// </summary>
-        LZXQ
+        兰州,
+        天水,
+        金昌,
+        白银,
+        武威,
+        平凉,
+        酒泉,
+        庆阳,
+        定西,
+        陇南,
+        临夏,
+        嘉峪关,
+        甘南,
+        张掖,
+        兰州新区
     }
+
     /// <summary>
-    /// 员工等级
+    /// 员工等级(由于C#枚举成员中不能包含括号，所以此处使用简写来代替)
     /// </summary>
-    public enum StaffLevel
+    public enum EmployeeLevel
     {
-        /// <summary>
-        /// 一级注册消防工程师
-        /// </summary>
-        YJXFGCS,
-        /// <summary>
-        /// 二级注册消防工程师
-        /// </summary>
-        EJXFGCS,
+        一级注册消防工程师,
+        二级注册消防工程师,
         /// <summary>
         /// 建（构）筑物消防员（初级）
         /// </summary>
-        CJXFY,
+        初级,
         /// <summary>
         /// 建（构）筑物消防员（中级）
         /// </summary>
-        ZJXFY,
+        中级,
         /// <summary>
         /// 建（构）筑物消防员（高级）
         /// </summary>
-        GJXFY
+        高级
     }
+
     /// <summary>
     /// 性别
     /// </summary>
     public enum Gender
     {
-        man,
-        woman,
-        unknow
+        男,
+        女,
+        未知
     }
 }
