403866460deca9fb4b8aff83465bd56dfe978f44
wangchz13 <wangchz13@gmail.com>
2017-05-18 12:59:41 +0800

    添加新的Controller,负责数据获取

---
+++ GSXF.Web/Controllers/DataController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GSXF.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace GSXF.Web.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public string GenderList()
+        {
+            var data = EnumToObject(typeof(Gender));
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public string EmployeeLevelList()
+        {
+            var data = EnumToObject(typeof(EmployeeLevel));
+            string str = JsonConvert.SerializeObject(data);
+            JArray obj = JArray.Parse(str);
+            foreach(var i in obj)
+            {
+                if(i["Text"].ToString() == "初级")
+                {
+                    i["Text"] = "建(构)筑物消防员(初级)";
+                }
+                else if(i["Text"].ToString() == "中级")
+                {
+                    i["Text"] = "建(构)筑物消防员(中级)";
+                }
+                else if (i["Text"].ToString() == "高级")
+                {
+                    i["Text"] = "建(构)筑物消防员(高级)";
+                }
+            }
+            return obj.ToString();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// 枚举转对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private object EnumToObject(Type type)
+        {
+            var data = Enum.GetNames(type).Select(text => new
+            {
+                Id = (int)Enum.Parse(type, text),
+                Text = text
+            }).ToArray();
+            return data;
+        }
+    }
+}
