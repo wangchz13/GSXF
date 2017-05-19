9b765a48cf2d1b13b71907fbb9cf5cea384675e6
wangchz13 <wangchz13@gmail.com>
2017-05-19 22:05:08 +0800

    用户表

---
+++ GSXF.Core/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace GSXF.Core
+{
+    /// <summary>
+    /// 用户类
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime LastLoginTime { get; set; }
+
+        public string LastLoginIP { get; set; }
+
+        public DateTime RegTime { get; set; }
+    }
+}
