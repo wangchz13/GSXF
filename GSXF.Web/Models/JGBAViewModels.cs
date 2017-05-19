6f16f9ff7a556ea435669d8026974f640c1a642c
wangchz13 <wangchz13@gmail.com>
2017-05-19 16:13:31 +0800

    trash

---
+++ GSXF.Web/Models/JGBAViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GSXF.Core;
+
+namespace GSXF.Web.Models
+{
+    public class JGBAViewModel
+    {
+        public Company Company { get; set; }
+        public IEnumerable<OfficeAddress> OfficeAddresses { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+}
