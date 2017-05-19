be99e6443e32428d62b2a64d370994d47e0fdecb
wangchz13 <wangchz13@gmail.com>
2017-05-19 16:13:07 +0800

    实现地址-人员-公司的系列存储

--- GSXF.Web/Controllers/HomeController.cs
+++ GSXF.Web/Controllers/HomeController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GSXF.Auxiliary;
+using Newtonsoft.Json;
 using GSXF.Core;
+
+
 
 namespace GSXF.Web.Controllers
 {
@@ -12,6 +15,10 @@ namespace GSXF.Web.Controllers
     {
         private static ArticleManager articleManager = new ArticleManager();
         private static EmployeeManager employeeManager = new EmployeeManager();
+        private static OfficeAddressManager officeAddressManager = new OfficeAddressManager();
+        private static CompanyManager companyManager = new CompanyManager();
+        private static int employeeRow = 0;
+        private static int officeAddressRow = 0;
         public ActionResult Default()
         {
             return View();
@@ -42,16 +49,52 @@ namespace GSXF.Web.Controllers
         {
             return View();
         }
-
+        /// <summary>
+        /// 添加人员信息
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public ActionResult AddEmployee(List<Employee> list)
         {
-            string result = "";
-            foreach(var i in list)
+            foreach (var i in list)
             {
                 employeeManager.Add(i);
-                
             }
-            return Json(new { rows = list.Count.ToString(), result = result });
+            employeeRow = list.Count;
+            return Json(new { rows = list.Count });
+        }
+        /// <summary>
+        /// 添加办公地址信息
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public ActionResult AddOfficeAddress(List<OfficeAddress> list)
+        {
+            foreach(var i in list)
+            {
+                officeAddressManager.Add(i);
+            }
+            officeAddressRow = list.Count;
+            return Json(new { rows = list.Count });
+        }
+
+        public ActionResult AddCompany(Company company)
+        {
+            if(employeeRow != 0)
+            {
+                var employeeList = employeeManager.FindList().OrderByDescending(m => m.ID).Take(employeeRow);
+                company.Employees = employeeList.ToList();
+                employeeRow = 0;
+            }
+            if(officeAddressRow != 0)
+            {
+                var addressList = officeAddressManager.FindList().OrderByDescending(m => m.ID).Take(officeAddressRow);
+                company.OfficeAddresses = addressList.ToList();
+                officeAddressRow = 0;
+
+            }
+            companyManager.Add(company);
+            return Json("success");
         }
     }
 }
