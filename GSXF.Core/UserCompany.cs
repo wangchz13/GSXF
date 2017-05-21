d96d23a9a6bce19bee274f08664754ce734e24ec
wangchz13 <wangchz13@gmail.com>
2017-05-21 15:54:09 +0800

    用户公司表

---
+++ GSXF.Core/UserCompany.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSXF.Core
+{
+    public class UserCompany
+    {
+        public int ID;
+        public int UserID;
+        public int CompanyID;
+    }
+}
