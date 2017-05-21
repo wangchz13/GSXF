aa1753721ef2fa895b78d11d766c97791221cc54
wangchz13 <wangchz13@gmail.com>
2017-05-21 15:57:35 +0800

    ---

---
+++ GSXF.Web/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GSXF.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public int InstitutionType { get; set; }
+    }
+}
