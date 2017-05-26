d1c3df6114352177973cc4207c29db42331b66f3
wangchz13 <wangchz13@gmail.com>
2017-05-26 17:52:52 +0800

    消防机构定义

---
+++ GSXF.Model/FireControlInstitution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace GSXF.Model
+{
+    public class FireControlInstitution
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
