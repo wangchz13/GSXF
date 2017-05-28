47124a7e58907bc0ac76d988da8d98f6595a7eca
wangchz13 <wangchz13@gmail.com>
2017-05-28 21:01:30 +0800

    Nothing

--- GSXF.Web/Models/LoginViewModel.cs
+++ GSXF.Web/Models/LoginViewModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace GSXF.Web.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="煞笔名字不能为空")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "煞笔密码不能为空")]
         public string Password { get; set; }
     }
 }
