4c5f1ed3f073b163e5fdab0d12ad0262c21913a3
wangchz13 <wangchz13@gmail.com>
2017-05-28 20:56:46 +0800

    实现了SHA256加密算法

---
+++ GSXF.Security/Encryption .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace GSXF.Security
+{
+    public class Encryption
+    {
+        public static string SHA256(string plainText)
+        {
+            SHA256Managed sha256 = new SHA256Managed();
+            byte[] cipherText = sha256.ComputeHash(Encoding.Default.GetBytes(plainText));
+            return System.Convert.ToBase64String(cipherText);
+        }
+    }
+}
