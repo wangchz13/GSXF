327e5a0d5e9c6ec2623985b0a40ac936cbbccd64
wangchz13 <wangchz13@gmail.com>
2017-05-28 08:17:45 +0800

    usecontroller

--- GSXF.Web/Controllers/UserController.cs
+++ GSXF.Web/Controllers/UserController.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using GSXF.Security;
 using GSXF.DataBase;
 using GSXF.Model;
+using GSXF.Web.Models;
+using GSXF.Auxiliary;
 
 namespace GSXF.Web.Controllers
 {
@@ -14,67 +19,90 @@ namespace GSXF.Web.Controllers
     {
         private static UserRoleManager userRoleManager = new UserRoleManager();
         private static RoleManager roleManager = new RoleManager();
-
-
+        private static UserManager userManager = new UserManager();
 
         // GET: User
         public ActionResult Index()
         {
-            var _user = Session["User"] as User;
-            var _roleID = userRoleManager.Find(ur => ur.UserID == _user.ID).RoleID;
-            var _roleName = roleManager.Find(_roleID).Name;
-            if (_roleName == "Root")
+            var user = Session["User"] as User;
+            var roleID = userRoleManager.Find(ur => ur.UserID == user.ID).RoleID;
+            var roleName = roleManager.Find(roleID).Name;
+            if (roleName == "Root")
                 return View("ROOT");
-            else if (_roleName == "消防机构总队")
+            else if (roleName == "消防机构总队")
                 return View("ZongDui");
-            else if (_roleName == "消防机构支队")
+            else if (roleName == "消防机构支队")
                 return View("ZhiDui");
-            else if (_roleName == "消防机构大队")
+            else if (roleName == "消防机构大队")
                 return View("ZongDui");
-            else if (_roleName == "服务机构")
+            else if (roleName == "服务机构")
                 return View("Company");
 
             return View();
         }
 
-        [AllowAnonymous]
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
+            ViewBag.ReturnUrl = returnUrl;
             return View();
         }
 
-        [AllowAnonymous]
         [HttpPost]
-        public ActionResult Login(string name, string password)
+        public ActionResult Login(LoginViewModel login, string returnUrl)
         {
-            return View();
+            Response resp = userManager.Verify(login.Name, login.Password);
+            if (resp.Code == 3)
+            {
+                var user = userManager.Find(login.Name);
+                user.LoginTime = DateTime.Now;
+                user.LoginIP = Request.UserHostAddress;
+                user.IsOnline = true;
+                userManager.Update(user);
+                Session.Add("User", user);
+                if (!string.IsNullOrEmpty(returnUrl))
+                    return Redirect(returnUrl);
+                else
+                    return RedirectToAction("Index");
+            }
+            return Json(resp);
         }
 
+        public ActionResult LoginOut()
+        { 
+            User user = Session["User"] as User;
+            if (user != null)
+            {
+                user.IsOnline = false;
+                userManager.Update(user);
+            }
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
         /// <summary>
         /// 超级管理员页面
         /// </summary>
         /// <returns></returns>
-        private ActionResult ROOT()
+        public ActionResult ROOT()
         {
             return View();
         }
 
-        private ActionResult ZongDui()
+        public ActionResult ZongDui()
         {
             return View();
         }
 
-        private ActionResult ZhiDui()
+        public ActionResult ZhiDui()
         {
             return View();
         }
 
-        private ActionResult DaDui()
+        public ActionResult DaDui()
         {
             return View();
         }
 
-        private ActionResult Company()
+        public ActionResult Company()
         {
             return View();
         }
