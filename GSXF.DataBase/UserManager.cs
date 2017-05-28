fd5784a2bdd8d7e3c132e303df492de8d5d1f398
wangchz13 <wangchz13@gmail.com>
2017-05-28 08:16:05 +0800

    使用Response

--- GSXF.DataBase/UserManager.cs
+++ GSXF.DataBase/UserManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GSXF.Model;
+using GSXF.Auxiliary;
+
 
 namespace GSXF.DataBase
 {
@@ -15,10 +17,31 @@ namespace GSXF.DataBase
             return Find(u => u.Name == name);
         }
 
-        public bool Verify(string name, string password)
+        public Response Verify(string name, string password)
         {
-            var user = Find(u => u.Name == name && u.Password == password);
-            return user != null;
+            Response _resp = new Response();
+            var _user = Find(u => u.Name == name);
+            if(_user == null)
+            {
+                _resp.Code = 0;
+                _resp.Message = "此账号不存在";
+            }
+            else if(_user.Password != password)
+            {
+                _resp.Code = 1;
+                _resp.Message = "密码错误";
+            }
+            else if(_user.IsOnline)
+            {
+                _resp.Code = 2;
+                _resp.Message = "当前用户已登录，无法再次登录";
+            }
+            else
+            {
+                _resp.Code = 3;
+                _resp.Message = "登录成功";
+            }
+            return _resp;
         }
     }
 }
