1be9569681fa492ac2d9bb9582c7452a72e1c51d
wangchz13 <wangchz13@gmail.com>
2017-05-28 08:18:01 +0800

    有关Session事件

--- GSXF.Web/Global.asax.cs
+++ GSXF.Web/Global.asax.cs
@@ -5,17 +5,40 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using GSXF.DataBase;
+using GSXF.Model;
+
 
 namespace GSXF.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        private static UserManager userManager = new UserManager();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session.Timeout = 1;
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            User user = Session["User"] as User;
+            //string name = user.Name;
+            //user = userManager.Find(name);
+            if(user != null)
+            {
+                user.IsOnline = false;
+                userManager.Update(user);
+            }
         }
     }
 }
