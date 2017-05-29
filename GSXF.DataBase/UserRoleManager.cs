f8bbdc3ccdf6be90b3eba68d2b24e03b7d04bf32
wangchz13 <wangchz13@gmail.com>
2017-05-29 13:55:51 +0800

    Add(int userID, int roleID)

--- GSXF.DataBase/UserRoleManager.cs
+++ GSXF.DataBase/UserRoleManager.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GSXF.Model;
+using GSXF.Auxiliary;
+
 
 namespace GSXF.DataBase
 {
     public class UserRoleManager : BaseManager<UserRole>
     {
-
+        public Response Add(int userID, int roleID)
+        {
+            UserRole userRole = new UserRole();
+            userRole.UserID = userID;
+            userRole.RoleID = roleID;
+            return Add(userRole);
+        }
     }
 }
