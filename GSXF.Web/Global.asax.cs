5cf6e8ef14f859bb969310dd258d33ceac850968
wangchz13 <wangchz13@gmail.com>
2017-05-29 14:00:06 +0800

    如果user表为空，则网站是第一次运行，调用FirstRun.Start()开始配置数据库表

--- GSXF.Web/Global.asax.cs
+++ GSXF.Web/Global.asax.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using GSXF.DataBase;
 using GSXF.Model;
 
@@ -15,13 +19,18 @@ namespace GSXF.Web
     {
 
         private static UserManager userManager = new UserManager();
-
+        private static FireControlInstitutionManager fireManager = new FireControlInstitutionManager();
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            if (userManager.FindList().Count() == 0)
+            {
+                FirstRun.Start();
+            }
+            
         }
 
         protected void Session_Start(object sender, EventArgs e)
