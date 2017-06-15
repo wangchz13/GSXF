0908e9b28870b97ba25ceabba92df59e98450fb6
wangchz13 <wangchz13@gmail.com>
2017-06-15 19:43:47 +0800

    精简用户认证

--- GSXF.Security/UserAuthorize.cs
+++ GSXF.Security/UserAuthorize.cs
@@ -1,10 +1,5 @@
-﻿using System;
+﻿using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
-using System.Linq;
-using System.Linq.Expressions;
-using GSXF.DataBase;
-using GSXF.Model;
 
 namespace GSXF.Security
 {
@@ -13,77 +8,18 @@ namespace GSXF.Security
     /// </summary>
     public class UserAuthorizeAttribute : AuthorizeAttribute
     {
-        private static UserManager userManager = new UserManager();
-        private static ActionManager actionManager = new ActionManager();
-        private static ActionRoleManager actionRoleManager = new ActionRoleManager();
-
-        public override void OnAuthorization(AuthorizationContext filterContext)
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            var user = filterContext.HttpContext.Session["User"] as User;
-            var controller = filterContext.RouteData.Values["controller"].ToString();
-            var action = filterContext.RouteData.Values["action"].ToString();
-
-            var isAllowed = IsAllowed(user, controller, action);
-            if (!isAllowed)
-            {
-                if(user == null)
-                {
-                    var returnUrl = filterContext.HttpContext.Server.UrlEncode(filterContext.HttpContext.Request.Url.AbsoluteUri);
-                    filterContext.Result = new RedirectResult("/User/Login?returnUrl=" + returnUrl);
-                }
-                else
-                {
-                    filterContext.RequestContext.HttpContext.Response.Write("无权访问");
-                    filterContext.RequestContext.HttpContext.Response.End();
-                }
-            }
+            if (httpContext.Session["User"] == null)
+                return false;
+            else
+                return true;
         }
 
-        private bool IsAllowed(User user, string controller, string action)
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
         {
-            var controllerAction = actionManager.Find(a => a.ControllerName == controller && a.ActionName == action);
-
-            //记录不存在，允许访问
-            if (controllerAction == null)
-            {
-                return true;
-            }
-
-            //Action允许所有人访问
-            if (controllerAction.IsAllowedNoneRoles)
-            {
-                return true;
-            }
-
-            //Action允许所有登录的用户访问
-            if (controllerAction.IsAllowedAllRoles)
-            {
-                return user != null;
-            }
-            //下面是Action对指定角色允许访问
-            //选出Action对应的角色
-            var actionRoles = actionRoleManager.FindList(ar => ar.ActionID == controllerAction.ID).ToList();
-            if(actionRoles.Count == 0)
-            {
-                return true;
-            }
-            if (user == null) return false;
-
-            //找到用户角色，此处不考虑多个角色问题
-            var userRole = user.Role;
-
-            //查找Action允许访问的角色
-            var allowedRoles = actionRoleManager.FindList(ar => ar.ActionID == controllerAction.ID && ar.IsAllowed==true).Select(a => a.RoleID).ToList();
-            if (allowedRoles.Count > 0)
-            {
-                foreach (var i in allowedRoles)
-                {
-                    if (i == userRole.ID)
-                        return true;
-                }
-                return false;
-            }
-            return false;
+            filterContext.RequestContext.HttpContext.Response.Write("用户未登录或登录信息过期，点此<a href='/User/Login' target='_self'>重新登录</a>！");
+            filterContext.RequestContext.HttpContext.Response.End();
         }
     }
 }
