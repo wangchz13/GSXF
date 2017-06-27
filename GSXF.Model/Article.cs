4eca2ff054e1dca6ba7cfe5a8ca653031560e617
wangchz13 <wangchz13@gmail.com>
2017-06-27 17:00:08 +0800

    文章Mode

--- GSXF.Model/Article.cs
+++ GSXF.Model/Article.cs
@@ -7,11 +7,21 @@ namespace GSXF.Model
     {
         [Key]
         public int ID { get; set; }
-
+        /// <summary>
+        /// 文章标题
+        /// </summary>
         public string Title { get; set; }
-
+        /// <summary>
+        /// 上传文件对象
+        /// </summary>
         public virtual UploadFile File { get; set; }
-
+        /// <summary>
+        /// 文章栏目
+        /// </summary>
         public Category Category { get; set; }
+        /// <summary>
+        /// 提交时间
+        /// </summary>
+        public DateTime CommitDate { get; set; }
     }
 }
