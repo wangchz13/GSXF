7b05523ea9b9b1785c820989630de40a31e34443
wangchz13 <wangchz13@gmail.com>
2017-06-27 17:02:00 +0800

    COntroller基类

---
+++ GSXF.Web/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GSXF.DataBase;
+
+namespace GSXF.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public static ProjectManager projectManager = new ProjectManager();
+        public static CompanyManager companyManager = new CompanyManager();
+        public static UserCompanyManager userCompanyManager = new UserCompanyManager();
+        public static EmployeeManager employeeManager = new EmployeeManager();
+        public static FireControlInstitutionManager fireManager = new FireControlInstitutionManager();
+        public static UserManager userManager = new UserManager();
+        public static RoleManager roleManager = new RoleManager();
+        public static EvaluationManager evaManager = new EvaluationManager();
+        public static ArticleManager articleManager = new ArticleManager();
+        public static FileManager fileManager = new FileManager();
+        public static EvaluationManager evaluationManager = new EvaluationManager();
+
+
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            // 错误日志编写    
+            string controllerNamer = filterContext.RouteData.Values["controller"].ToString();
+            string actionName = filterContext.RouteData.Values["action"].ToString();
+            string exception = filterContext.Exception.ToString();
+            // 执行基类中的OnException    
+            base.OnException(filterContext);
+        }
+    }
+}
